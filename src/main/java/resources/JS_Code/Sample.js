5e5e554d2241d6cccede91c5e74f035cf8738e76
Raj Vasikarla <raj_vasikarla@intuit.com>
2015-05-06 09:14:48 -0700

    getting rid of a function from sample.js

--- src/main/java/resources/JS_Code/Sample.js
+++ src/main/java/resources/JS_Code/Sample.js
@@ -1,8 +1,3 @@
 var fun1 = function(name) {
     return 'Hello from Javascript, ' + name;
 };
-
-var fun2 = function (object) {
-    print("JS Class Definition: " + Object.prototype.toString.call(object));
-    print("###: " + Object.prototype.toString.call.getFirst_name());
-};
